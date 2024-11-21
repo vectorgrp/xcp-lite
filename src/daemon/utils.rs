1f272c25b22d15719cad5ac85ffc9eff424aa28d
GjergjiShkurti <gjergji.shkurti@vector.com>
2024-11-21 10:12:38 +0100

    fix dependency conflicts between windows and unix builds

--- src/daemon/utils.rs
+++ src/daemon/utils.rs
@@ -1,52 +1,52 @@
 use crate::daemon::error::DaemonizationError;
-use crate::daemon::platform::{Facility, Formatter3164};
 use crate::daemon::IoError;
-
-use libc::dup2;
-use std::{io::Write, os::fd::RawFd};
+use std::io::Write;
 
 #[cfg(unix)]
-#[inline]
-// Helper function to use syslog for logging
-pub(crate) fn setup_syslog(name: &str) {
-    let formatter = Formatter3164 {
-        facility: Facility::LOG_DAEMON,
-        hostname: None,
-        process: name.to_string(),
-        pid: 0,
-    };
+pub mod unixutils {
+    use super::*;
 
-    let logger = syslog::unix(formatter).expect("Failed to connect to syslog");
-    log::set_boxed_logger(Box::new(syslog::BasicLogger::new(logger))).expect("Failed to set logger");
-    log::set_max_level(log::LevelFilter::Info);
-}
+    use crate::daemon::platform::unixdeps::{dup2, Facility, Formatter3164, RawFd};
 
-#[cfg(unix)]
-#[inline]
-// Helper function to redirect file descriptors (stdin, stdout, stderr) to /dev/null
-pub(crate) unsafe fn redirect_fd(source: RawFd, target: RawFd) -> Result<(), DaemonizationError> {
-    if dup2(source, target) < 0 {
-        Err(DaemonizationError::Dup2Failed(IoError::last_os_error()))
-    } else {
+    #[inline]
+    // Helper function to use syslog for logging
+    pub(crate) fn setup_syslog(name: &str) {
+        let formatter = Formatter3164 {
+            facility: Facility::LOG_DAEMON,
+            hostname: None,
+            process: name.to_string(),
+            pid: 0,
+        };
+
+        let logger = syslog::unix(formatter).expect("Failed to connect to syslog");
+        log::set_boxed_logger(Box::new(syslog::BasicLogger::new(logger))).expect("Failed to set logger");
+        log::set_max_level(log::LevelFilter::Info);
+    }
+
+    #[inline]
+    // Helper function to redirect file descriptors (stdin, stdout, stderr) to /dev/null
+    pub(crate) unsafe fn redirect_fd(source: RawFd, target: RawFd) -> Result<(), DaemonizationError> {
+        if dup2(source, target) < 0 {
+            Err(DaemonizationError::Dup2Failed(IoError::last_os_error()))
+        } else {
+            Ok(())
+        }
+    }
+
+    #[inline]
+    // Helper function to create a PID file
+    pub(crate) fn open_pid_file(name: &str, pid: &i32) -> Result<(), DaemonizationError> {
+        let path = format!("/var/run/{}.pid", name);
+        let mut file = std::fs::File::create(&path).map_err(|_| DaemonizationError::OpenPidFileFailed)?;
+        file.write_all(pid.to_string().as_bytes()).map_err(|_| DaemonizationError::WriteToPidFileFailed)?;
         Ok(())
     }
-}
 
-#[cfg(unix)]
-#[inline]
-// Helper function to create a PID file
-pub(crate) fn open_pid_file(name: &str, pid: &i32) -> Result<(), DaemonizationError> {
-    let path = format!("/var/run/{}.pid", name);
-    let mut file = std::fs::File::create(&path).map_err(|_| DaemonizationError::OpenPidFileFailed)?;
-    file.write_all(pid.to_string().as_bytes()).map_err(|_| DaemonizationError::WriteToPidFileFailed)?;
-    Ok(())
-}
-
-#[cfg(unix)]
-#[inline]
-// Helper function to remove a PID file
-pub(crate) fn remove_pid_file(name: &str) -> Result<(), DaemonizationError> {
-    let path = format!("/var/run/{}.pid", name);
-    std::fs::remove_file(&path).map_err(|_| DaemonizationError::ClosePidFileFailed)?;
-    Ok(())
+    #[inline]
+    // Helper function to remove a PID file
+    pub(crate) fn remove_pid_file(name: &str) -> Result<(), DaemonizationError> {
+        let path = format!("/var/run/{}.pid", name);
+        std::fs::remove_file(&path).map_err(|_| DaemonizationError::ClosePidFileFailed)?;
+        Ok(())
+    }
 }
